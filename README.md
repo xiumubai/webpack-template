6a159552177d1aa86c22cc7540b76e8ae18b9a1f
xiumubai <1547702880@qq.com>
2022-04-23 22:53:26 +0800

    feat: npm scripts

--- README.md
+++ README.md
@@ -6,3 +6,8 @@ https://itxiaohao.github.io/passages/webpack4-pack-js/
 
 [] - 搭建项目并打包 JS 文件
 [] - 生产和开发模式
+
+- 打包第一个文件
+- 配置模块(webpack.config.js)
+- npm scripts
+-
--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import _ from 'lodash';
 function component() {
   const element = document.createElement('div');
 
-  element.innerHTML = _.join(['hello', 'webpack', '']);
+  element.innerHTML = _.join(['hello', 'webpack', 'scripts']);
 
   return element;
 }
