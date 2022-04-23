3a4fa3bb0d4d318a1a7f59900df7c751337b4e2a
xiumubai <1547702880@qq.com>
2022-04-23 23:29:11 +0800

    feat: 设置多入口，打包 index.js print.js

--- README.md
+++ README.md
@@ -9,9 +9,15 @@ https://itxiaohao.github.io/passages/webpack4-pack-js/
 - 打包第一个文件
 - 配置模块(webpack.config.js)
 - npm scripts
--
 
 ## 管理资源
 
 - 加载 css 设置 rules style-loader css-loader
 - 加载 images 图像
+
+## 管理输出
+
+到目前为止，我们都是在 index.html 文件中手动引入所有资源，然而随着应用程序增长，并且一旦开始 在文件名中使用 hash 并输出 多个 bundle，如果继续手动管理 index.html 文件，就会变得困难起来。然而，通过一些插件可以使这个过程更容易管控
+
+- 设置多入口，打包 index.js print.js
+- 设置 HtmlWebpackPlugin
--- src/index.js
+++ src/index.js
@@ -1,14 +1,18 @@
 import _ from 'lodash';
+import printMe from './print';
 import './style.css';
 import Icon from './1.jpg';
-import { electron } from 'webpack';
 
 function component() {
   const element = document.createElement('div');
+  const btn = document.createElement('button');
 
-  element.innerHTML = _.join(['hello', 'webpack', 'scripts']);
+  element.innerHTML = _.join(['hello', 'webpack', 'print']);
   element.classList.add('hello');
 
+  btn.innerHTML = 'click me and check the console';
+  btn.onclick = printMe;
+  element.appendChild(btn);
   const myIcon = new Image();
   myIcon.src = Icon;
   element.appendChild(myIcon);
